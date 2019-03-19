7679d6f12ed9c8a998b8d891c3b2dae0d60e3034
Andrei Ursulean <andreiursulean98@gmail.com>
2019-03-19 20:41:27 +0200

    Update main.m

--- src/main.m
+++ src/main.m
@@ -8,12 +8,17 @@ RADIUS = 100;
 HEIGHT = 100;
 
 ANTENNA_ANGLE = deg2rad(1);
-
+RESPONSE_TIME = 21e-6
 
 % % % % % RUN SIMULATION N TIMES% % % % % 
 for i = NUMBER_OF_SIMS
     [r, phi, z] = generate_acft(i, RADIUS, HEIGHT);
 
-%     TODO add simulation code
+    for j = 1:numel(phi)
+        for k = 1:numel(phi)
+            if (phi(k) - phi(j)) <= ANTENNA_ANGLE
+                % TODO add step 4
+            end
+        end
+    end
 end
- 
