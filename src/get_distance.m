70ecb80f045c1050b34ce25d5d0181be5821a56d
Andrei Ursulean <andreiursulean98@gmail.com>
2019-03-19 21:23:41 +0200

    Update get_distance.m
    added doc for get_distance

--- src/get_distance.m
+++ src/get_distance.m
@@ -1,9 +1,13 @@
 function distance = get_distance(r, phi, z)
-%GET_DISTANCE returns distance between two objects represented in
+%GET_DISTANCE returns distance from radar to aircraft represented in
 %cylindrical coordinates
+% r = horizontal distance from radar station
+% phi = azimuth of the aircraft
+% z = height of the aircraft
 
     x = r * cos(phi);
     y = r * sin(phi);
 
     distance = sqrt(x ^ 2 + y ^ 2 + z ^ 2);
 end
+
