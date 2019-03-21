d0f6c18a34a8c9ed75791b90e39c5c8b9463e5d2
Andrei Ursulean <andreiursulean98@gmail.com>
2019-03-21 10:19:35 +0200

    Update plot_scope.m

--- src/plot_scope.m
+++ src/plot_scope.m
@@ -1,5 +1,7 @@
-function plot_scope(radius, height, plane_radius, plane_azimuth, plane_height)
+function plot_scope(radius, height, plane_radius, plane_azimuth, plane_height, phi, fov)
 %PLOT_SCOPE plots radar scope
+    clf;
+
     [x, y, z] = cylinder(radius, 100);
     
     z = z * height;
@@ -9,4 +11,28 @@ function plot_scope(radius, height, plane_radius, plane_azimuth, plane_height)
     
     hold on;
     plot3(plane_radius .* cos(plane_azimuth), plane_radius .* sin(plane_azimuth), plane_height, 'go');
+    
+    x1 = [0, radius * cos(phi);
+        0, radius * cos(phi)];
+    
+    y1 = [0 radius * sin(phi);
+        0 radius * sin(phi)];
+    
+    z1 = [0 0;
+        height height];
+    
+    plane1 = surf(x1, y1, z1);
+    set(plane1, 'FaceAlpha', 0.2);
+    
+    x2 = [0, radius * cos(phi + fov);
+        0, radius * cos(phi + fov)];
+    
+    y2 = [0 radius * sin(phi + fov);
+        0 radius * sin(phi + fov)];
+    
+    z2 = [0 0;
+        height height];
+    
+    plane2 = surf(x2, y2, z2);
+    set(plane2, 'FaceAlpha', 0.2);
 end
